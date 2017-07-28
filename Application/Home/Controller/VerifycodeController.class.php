a6608929cdfe224e9c8c5636a0110427e5b8043b
QH-20120726RLNO\Administrator <talent2928@163.com>
2017-07-28 08:52:14 +0800

    修改

--- Application/Home/Controller/VerifycodeController.class.php
+++ Application/Home/Controller/VerifycodeController.class.php
@@ -13,7 +13,7 @@ class VerifycodeController extends Controller {
 
     public function check_verify()
     {
-        //print_r($_SESSION);
+        print_r($_SESSION);
         $code = I("verifycode");
         $verify = new \Think\Verify();
         if($verify->check($code)){
