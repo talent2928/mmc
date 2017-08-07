869e3d3bd64c23daa85813ac1b33c49e1cd1b1c0
wangliang <talent2928@163.com>
2017-08-07 19:06:10 +0800

    提交错误

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class TestController extends Controller {
     public function test(){
         $arraylist = new \Org\Util\ArrayList();
-        $b = 11;
+        $b = 121;
         $test_logic = new \Home\Logic\TestLogic();
         //$admin = new \Admin\Controller\TestController();
         //$admin = A('Admin/Test');
